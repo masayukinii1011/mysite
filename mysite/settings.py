a40aa15092ad0ae51f978193f8db44518714d65d
masayukinii1011 <masayukinii1011@gmail.com>
2020-02-22 16:03:03 +0900

    SECRET_KEYは別ファイルへ

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yg!4o#y797^ns1=7szh6@&%yq&%0n^i!7sql!zh65mx!f$aq1z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
